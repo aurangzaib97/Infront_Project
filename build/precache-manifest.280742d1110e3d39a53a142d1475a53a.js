0f159b494679cd6867c257aa3b7d836118f158aa
Aurangzaib Manzoor <aurangzaibmanzoor369@gmail.com>
2019-10-13 18:01:49 -0700

    new files

---
+++ build/precache-manifest.280742d1110e3d39a53a142d1475a53a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ba5e706ffa7db8ef58a1ae74f29691",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d867b3e78fed13e4edd",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "d32818746c386be56ebe",
+    "url": "/static/css/main.0df56c3a.chunk.css"
+  },
+  {
+    "revision": "1d867b3e78fed13e4edd",
+    "url": "/static/js/2.ee467955.chunk.js"
+  },
+  {
+    "revision": "d32818746c386be56ebe",
+    "url": "/static/js/main.ffd5d16d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
